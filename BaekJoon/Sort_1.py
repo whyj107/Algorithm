4a6929a36970b8502088bbcfdfb8897cb96af9b8
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-03-01 20:19:14 +0900

    Update Sort_1.py

--- BaekJoon/Sort_1.py
+++ BaekJoon/Sort_1.py
@@ -1,16 +1,5 @@
 answer = []
 
-for i in range(int(input())):
-    n = int(input())
-    answer.append(n)
-
-# insertion_sort(answer)
-answer.sort()
-
-for i in answer:
-    print(i)
-
-
 # 삽입 정렬
 # 정렬 범위를 1칸씩 확장해나가면서 새롭게 정렬 범위에 들어온 값을 기존값들과 비교하여
 # 알맞은 자리에 꼽아주는 알고리즘
@@ -21,3 +10,14 @@ def insertion_sort(arr):
                 arr[j], arr[j-1] = arr[j-1], arr[j]
             else:
                 break
+
+for i in range(int(input())):
+    n = int(input())
+    answer.append(n)
+
+insertion_sort(answer)
+# answer.sort()
+
+for i in answer:
+    print(i)
+
