642a5bf56e6c97763dfac5b72808475c6bdba423
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-03-01 21:23:52 +0900

    Update Sort_3.py

--- BaekJoon/Sort_3.py
+++ BaekJoon/Sort_3.py
@@ -24,4 +24,15 @@ counting_sort(answer)
 
 for i in answer:
     print(i)
+#---------------------------------------------------
+import sys
+series = [0] * 10001
 
+for i in range(int(input())):
+    a = int(sys.stdin.readline())
+    series[a] = series[a] + 1
+
+for b in range(len(series)):
+    if series[b] !=0:
+        for c in range(series[b]):
+            print(b)
