abed2b6acf0d725c3f06ec84664583eb649c647e
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-03-17 22:45:25 +0900

    CodeWar

--- CodeWar/20200316-20200317_Snail.py
+++ CodeWar/20200316-20200317_Snail.py
@@ -27,10 +27,15 @@
 
 # --------------------------
 # 어렵다 어려워
+# del 리스트[인덱스] : 리스트의 해당 인덱스 원소 삭제
+# 리스트.remove(원소) : 리스트 a의 해당 원소 삭제
+# zip(*iterable) : 동일한 개수로 이루어진 자료형을 묶어 주는 역할
+#                  list(zip([1, 2, 3], [4, 5, 6]))
+#                  ==> [(1, 4), (2, 5), (3, 6)]
 # --------------------------
 
 # My Code
-def snail(snail_map):
+def snail_(snail_map):
     result = []
     while len(snail_map) > 0:
         # 오른쪽으로
@@ -53,6 +58,14 @@ def snail(snail_map):
                 result += [i[0]]
                 del i[0]
     return result
+
+# Warriors Code
+def snail(array):
+    out = []
+    while len(array):
+        out += array.pop(0)
+        array = list(zip(*array))[::-1]
+    return out
 
 if __name__=='__main__':
     answer = snail([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
