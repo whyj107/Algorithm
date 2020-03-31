c1dca4e371dd8bdf9356034a548df881793acd67
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-03-31 20:39:41 +0900

    BaekJoon

---
+++ BaekJoon/Start_and_Link.py
@@ -0,0 +1,43 @@
+# 조합 함수
+from itertools import combinations
+
+def solv(N, S, team):
+    # 갭이 가장 작은 값을 찾기 위하여
+    min_tmp = 99999
+
+    for i in range(len(team) // 2):
+        # start 팀
+        team_start = team[i]
+        start_sum = 0
+
+        # link 팀
+        team_link = team[-i - 1]
+        link_sum = 0
+        for j in range(N // 2):
+            member_s = team_start[j]
+            member_l = team_link[j]
+            # start 팀
+            for k in team_start:
+                start_sum += S[member_s][k]
+            # link 팀
+            for k in team_link:
+                link_sum += S[member_l][k]
+
+        # 최솟값 찾기
+        min_tmp = min(min_tmp, abs(start_sum - link_sum))
+
+    return min_tmp
+
+
+if __name__=='__main__':
+    N = int(input())
+    S = [list(map(int, input().split())) for _ in range(N)]
+    members = [i for i in range(N)]
+    team = []
+
+    # 조합으로 가능한 팀 생성
+    for x in list(combinations(members, N // 2)):
+        team.append(x)
+
+    answer = solv(N, S, team)
+    print(answer)
