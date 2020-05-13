494fb1b37e82e23d306e858b121c1c857a7a3f69
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-05-13 21:07:07 +0900

    CodeWar

---
+++ CodeWar/20200513_Basic_variable_assignment.py
@@ -0,0 +1,9 @@
+# 문제
+# Basic variable assignment
+# This code should store "codewa.rs" as a variable called name
+# but it's not working. Can you figure out why?
+
+a = "code"
+b = "wa.rs"
+name = a + b
+print(name)
