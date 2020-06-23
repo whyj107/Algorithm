90075e0bc2c65b35c3462eb115e34869322bba52
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-06-23 13:47:57 +0900

    Programmers

---
+++ Programmers/20200623_K번째수.py
@@ -0,0 +1,24 @@
+# 문제
+# K번째수
+# https://programmers.co.kr/learn/courses/30/lessons/42748?language=python3
+
+# 나의 풀이
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        i = c[0]
+        j = c[1]
+        k = c[2]
+
+        tmp = array[i-1:j]
+        tmp.sort()
+        answer.append(tmp[k-1])
+    return answer
+
+# 다름 사람의 풀이
+def solution(array, commands):
+    return list(map(lambda x: sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+
+if __name__ == '__main__':
+    print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]), [5, 6, 3])
