794dc6c98c22e9029bd9fddb1a2199ac00eff643
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-09-06 15:42:07 +0900

    review

--- Programmers/20200707_문자열을 정수로 바꾸기.py
+++ Programmers/20200707_문자열을 정수로 바꾸기.py
@@ -9,13 +9,9 @@ def solution(s):
 # 다른 사람의 풀이
 def strToInt(str):
     result = 0
-
     for idx, number in enumerate(str[::-1]):
-        if number == '-':
-            result *= -1
-        else:
-            result += int(number) * (10 ** idx)
-
+        if number == '-': result *= -1
+        else: result += int(number) * (10 ** idx)
     return result
 
 if __name__ == '__main__':
