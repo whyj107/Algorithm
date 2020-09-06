dfd0c66b08a87c73af7481de341523f864528d02
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-09-06 15:42:12 +0900

    review

--- Programmers/20200717_키패드 누르기.py
+++ Programmers/20200717_키패드 누르기.py
@@ -4,8 +4,7 @@
 
 # 나의 풀이
 def solution(numbers, hand):
-    answer = ''
-    l, r = '*', '#'
+    answer, l, r = '', '*', '#'
     for i in numbers:
         if i in [1, 4, 7]:
             answer += "L"
@@ -16,24 +15,19 @@ def solution(numbers, hand):
         elif i in [2, 5, 8, 0]:
             l_cnt, r_cnt = 0, 0
 
-            if l in [2, 5, 8, 0]:
-                l_cnt = abs([2, 5, 8, 0].index(i) - [2, 5, 8, 0].index(l))
-            else:
-                l_cnt = abs([2, 5, 8, 0].index(i) - [1, 4, 7, '*'].index(l))+1
-            if r in [2, 5, 8, 0]:
-                r_cnt = abs([2, 5, 8, 0].index(i) - [2, 5, 8, 0].index(r))
-            else:
-                r_cnt += abs([2, 5, 8, 0].index(i) - [3, 6, 9, '#'].index(r))+1
+            if l in [2, 5, 8, 0]: l_cnt = abs([2, 5, 8, 0].index(i) - [2, 5, 8, 0].index(l))
+            else: l_cnt = abs([2, 5, 8, 0].index(i) - [1, 4, 7, '*'].index(l))+1
+
+            if r in [2, 5, 8, 0]: r_cnt = abs([2, 5, 8, 0].index(i) - [2, 5, 8, 0].index(r))
+            else: r_cnt += abs([2, 5, 8, 0].index(i) - [3, 6, 9, '#'].index(r))+1
 
             if l_cnt < r_cnt:
                 answer += 'L'
                 l = i
             elif l_cnt == r_cnt:
                 answer += hand[0].upper()
-                if hand[0].upper() == 'L':
-                    l = i
-                else:
-                    r = i
+                if hand[0].upper() == 'L': l = i
+                else: r = i
             else:
                 answer += 'R'
                 r = i
