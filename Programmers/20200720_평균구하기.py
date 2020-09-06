0f3a0904ab4f79729fc3fd3ea05272c0f5ff61b0
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-09-06 15:42:17 +0900

    review

--- Programmers/20200720_평균구하기.py
+++ Programmers/20200720_평균구하기.py
@@ -9,8 +9,7 @@ def solution(arr):
 # 다른 사람의 풀이
 from functools import reduce
 def average(list):
-    # 함수를 완성해서 매개변수 list의 평균값을 return하도록 만들어 보세요.
-    return reduce(lambda x, y : x + y, list) / len(list)
+    return reduce(lambda x, y: x + y, list) / len(list)
 
 if __name__ == '__main__':
     print(solution([1, 2, 3, 4]), 2.5)
