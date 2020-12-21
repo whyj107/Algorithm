9362c2713298806d6c1c45bbe1d7c0ec5320e72d
whyj107 <60024292+whyj107@users.noreply.github.com>
2020-12-22 08:48:51 +0900

    CodeWar

---
+++ CodeWar/20201222_Vowel Count.py
@@ -0,0 +1,16 @@
+# Vowel Count
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
+
+# 나의 풀이
+def get_count(input_str):
+    return sum(1 for i in input_str if i in 'aeiou')
+
+# 다른 사람의 풀이
+def getCount1(s):
+    return sum(c in 'aeiou' for c in s)
+
+print(get_count("abracadabra"), 5)
+print(get_count(""), 0)
+print(get_count("pear tree"), 4)
+print(get_count("o a kak ushakov lil vo kashu kakao"), 13)
+print(get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), 168)
