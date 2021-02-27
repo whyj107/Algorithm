955236ea22bc9dc083cf3de46a7ba15b04f2fd91
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-02-27 19:06:41 +0900

    CodeWar

---
+++ CodeWar/20210227_Convert a Boolean to a String.py
@@ -0,0 +1,9 @@
+# Convert a Boolean to a String
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+
+# 나의 풀이
+def boolean_to_string(b):
+    return str(b)
+
+print(boolean_to_string(True), "True")
+print(boolean_to_string(False), "False")
